a217321bb038db24f8efe7c108d552cca3279b74
Alexandr Romanov <aromanov07@gmail.com>
2022-10-29 02:02:26 +0300

    Up wathOS version to 9.0

--- Package.swift
+++ Package.swift
@@ -28,7 +28,7 @@ let package = Package(
         .iOS(.v15),
         .macOS(.v12),
         .tvOS(.v15),
-        .watchOS(.v8),
+        .watchOS(.v9),
     ],
     products: [
         .library(name: "OversizeHealthService", targets: ["OversizeHealthService"]),
