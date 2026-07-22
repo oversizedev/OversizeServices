d93dc25e2113c0a76b76c24f982b66f793317d65
Alexander Romanov <aromanov07@gmail.com>
2026-07-22 14:50:17 +0300

    Fix LinkMetadataService availability for tvOS (#11)
    ## Summary
    `LPMetadataProvider` from LinkPresentation is only available on tvOS 18,
    but `LinkMetadataService` (and its `Container` registration) were
    annotated `tvOS 16.0`. This breaks tvOS builds of any package depending
    on `OversizeWebService` (e.g. OversizeKit NoticeKit/NotificationKit
    schemes).
    
    ## Changes
    - Raise `LinkMetadataService` availability to `tvOS 18.0`.
    - Match the `Container.linkMetadataService` registration to `tvOS 18.0`.
    
    ## Verification
    Fixes tvOS compile errors: `'LPMetadataProvider' is only available in
    tvOS 18.0 or newer` in
    `Sources/OversizeWebService/LinkMetadataService.swift`.

--- Sources/OversizeWebService/ServiceRegistering.swift
+++ Sources/OversizeWebService/ServiceRegistering.swift
@@ -7,7 +7,7 @@
 import FactoryKit
 import Foundation
 
-@available(macOS 13.0, iOS 16.0, tvOS 16.0, *)
+@available(macOS 13.0, iOS 16.0, tvOS 18.0, *)
 public extension Container {
     var linkMetadataService: Factory<LinkMetadataService> {
         self { LinkMetadataService() }
